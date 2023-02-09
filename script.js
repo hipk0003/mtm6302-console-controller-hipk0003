36e6985aab1c57d46cfafc2e27e635a11abf3d2a
Chris Hipkiss <hipk0003@algonquinlive.com>
2023-02-08 19:01:31 -0500

    changes

---
+++ script.js
@@ -0,0 +1,20 @@
+function setTitle (){
+    const title = document.getElementById ("title").innerHTML = "New title";
+}
+setTitle ()
+
+function setDescription(){
+    const description = document.getElementById("description").innerHTML = "This is the new description for the console"
+}
+setDescription ()
+
+function setBackgroundColor(){
+    const background = document.body.style.backgroundColor = ("darkgreen")
+ } 
+ setBackgroundColor ()
+
+ function setFontColor (){
+    const font = document.getElementById ("main").style.color = "white";
+}
+setFontColor ()
+
