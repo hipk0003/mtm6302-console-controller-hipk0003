cac448dabcbe159263fb320bb4a2e9a77fd6a659
Chris Hipkiss <hipk0003@algonquinlive.com>
2023-02-16 07:53:52 -0500

    done assignment

--- script.js
+++ script.js
@@ -12,9 +12,19 @@ function setBackgroundColor(){
     const background = document.body.style.backgroundColor = ("darkgreen")
  } 
  setBackgroundColor ()
+//  when i comment out the function above, my "setTheme" function works.
 
  function setFontColor (){
     const font = document.getElementById ("main").style.color = "white";
 }
 setFontColor ()
+
+function setTheme (){
+    let element = document.body;
+    element.classList.toggle('dark')
+}
+
+
+
+
 
